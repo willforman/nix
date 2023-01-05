d5a565c66d6e4cb106272b56cef2a504dc149b87
Will Forman <wf8581@gmail.com>
2023-01-05 16:44:13 +0000

    Update home manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -33,23 +33,28 @@
     };
   };
 
-  # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "will";
+    homeDirectory = "/home/will";
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
-
-  # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "willforman";
+    userEmail = "wf8581@gmail.com";
+  };
+
+  programs.tmux.enable = true;
+  programs.lazygit.enable = true;
+
+  home.packages = with pkgs; [
+    difftastic
+  ];
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "22.11";
 }
