9490566e08308a236c937c98099682ed05b9513d
Will Forman <wf8581@gmail.com>
2023-01-06 22:10:44 +0000

    Add neovim

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,9 +7,8 @@
     # If you want to use home-manager modules from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModule
 
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
     ./zsh/zsh.nix
+    ./nvim/nvim.nix
   ];
 
   nixpkgs = {
