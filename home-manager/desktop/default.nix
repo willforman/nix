5a37c905d20fde1ae5e4f46d11515f6c288c2f0d
willforman <wf8581@gmail.com>
2023-03-26 18:48:36 -0400

    Update structure and add clipboard support

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -4,44 +4,4 @@
     ../lib/graphical_apps.nix
     ../lib/linux/graphical_apps
   ];
-  
-  wayland.windowManager.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-    config = {
-      modifier = "Mod4";
-      terminal = "kitty";
-      menu = "wofi --show run";
-      bars = [];
-      output = {
-        e-DP-1 = {
-          scale = "1";
-        };
-      };
-    };
-  };
-
-  programs.waybar = {
-    enable = true;
-    systemd.enable = true;
-    settings = {
-      mainBar = {
-        modules-left = [ "sway/workspaces" ];
-        modules-center = [ "sway/window" ];
-        modules-right = [ "temperature" "disk" "cpu" "memory" "clock" ];
-      };
-    };
-  };
-
-  home.packages = with pkgs; [
-    dmenu
-    waybar
-    wofi
-    mako
-  ];
-
-  home.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = 1;
-    KITTY_ENABLE_WAYLAND = 1;
-  };
 }
--- home-manager/lib/linux/graphical_apps/default.nix
+++ home-manager/lib/linux/graphical_apps/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./sway.nix
     ./lock_screen.nix
   ];
 
