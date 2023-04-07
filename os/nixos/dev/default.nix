588ec27b7c633551d1e87b5fc989a5f511cb10cb
Will Forman <wf8581@gmail.com>
2023-04-07 15:42:43 -0400

    [dev] turn off screen after inactivity

--- os/nixos/dev/default.nix
+++ os/nixos/dev/default.nix
@@ -9,4 +9,8 @@
 
   # Keep laptop running when lid is shut
   services.logind.lidSwitchExternalPower = "ignore";
+
+  boot.kernelParams = [ 
+    "consoleblank=300" # Turn off screen in TTY
+  ];
 }
