a34f14a30ee8c75aea79ac9d184801c2a27d8518
willforman <wf8581@gmail.com>
2023-04-07 13:52:07 -0400

    [dev] use latest kernel

--- os/nixos/dev/default.nix
+++ os/nixos/dev/default.nix
@@ -9,6 +9,8 @@
 
   time.timeZone = "America/New_York";
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Keep laptop running when lid is shut
   services.logind.extraConfig = ''
     HandleLidSwitch=ignore
