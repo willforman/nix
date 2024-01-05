6f83e934f9c7e3d37c276636adae1381930797a5
willforman <wf8581@gmail.com>
2024-01-04 21:42:33 -0600

    Add dap-ui and try adding dap pretty printing

--- home-manager/lib/nvim/config/lua/config/plugins/dap.lua
+++ home-manager/lib/nvim/config/lua/config/plugins/dap.lua
@@ -1,6 +1,9 @@
 local M = {
   'mfussenegger/nvim-dap',
   event = 'VeryLazy',
+  dependencies = {
+    "rcarriga/nvim-dap-ui"
+  }
 }
 
 
@@ -156,6 +159,22 @@ function M.config()
       }
     }
   }
+
+  local dapui = require("dapui")
+  dapui.setup()
+
+  dap.listeners.before.attach.dapui_config = function()
+    dapui.open()
+  end
+  dap.listeners.before.launch.dapui_config = function()
+    dapui.open()
+  end
+  dap.listeners.before.event_terminated.dapui_config = function()
+    dapui.close()
+  end
+  dap.listeners.before.event_exited.dapui_config = function()
+    dapui.close()
+  end
 end
 
 return M
