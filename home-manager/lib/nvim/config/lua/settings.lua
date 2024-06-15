c2cbcc07ff3f49494e77989a96e4a6c0c002f3f4
willforman <wf8581@gmail.com>
2024-06-15 13:32:53 -0500

    [nvim] Enable relative lines and center cursor on <c-d>/<c-u>

--- home-manager/lib/nvim/config/lua/settings.lua
+++ home-manager/lib/nvim/config/lua/settings.lua
@@ -6,4 +6,8 @@ vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.autoindent = true
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
