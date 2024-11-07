17b696076ae951d4a6b959f24cb9c955406640a9
willforman <wf8581@gmail.com>
2024-11-06 18:20:20 -0600

    nvim: Add more cattpuccin plugins

--- home-manager/lib/nvim/config/lua/plugins/catppuccin.lua
+++ home-manager/lib/nvim/config/lua/plugins/catppuccin.lua
@@ -8,12 +8,35 @@ function M.config()
   require('catppuccin').setup({
     flavour = "mocha",
     integrations = {
-      cmp = true,
-      gitsigns = true,
       alpha = true,
+      cmp = true,
+      dap = true,
+      dap_ui = true,
+      diffview = true,
+      gitsigns = true,
+      lsp_trouble = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { 'italic' },
+          hints = { 'italic' },
+          warnings = { 'italic' },
+          information = { 'italic' },
+          ok = { 'italic' },
+        },
+        underlines = {
+          errors = { 'underline' },
+          hints = { 'underline' },
+          warnings = { 'underline' },
+          information = { 'underline' },
+          ok = { 'underline' },
+        },
+        inlay_hints = {
+          background = true,
+        }
+      },
       treesitter = true,
       telescope = { enabled = true },
-      lsp_trouble = true,
       which_key = true,
     },
   })
