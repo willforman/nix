9ecdfbda1bfd3ff8f3892cc7bcefed2fe7016f75
willforman <wf8581@gmail.com>
2024-11-06 18:16:25 -0600

    alacritty: Update colors to latest from cattpuccin

--- home-manager/lib/graphical_apps.nix
+++ home-manager/lib/graphical_apps.nix
@@ -42,52 +42,8 @@ in
         primary = {
           background = "#1E1E2E";
           foreground = "#CDD6F4";
-          dim_foreground = "#CDD6F4";
+          dim_foreground = "#7F849C";
           bright_foreground = "#CDD6F4";
-        };
-        normal = {
-          black = "#45475A";
-          red = "#F38BA8";
-          green = "#A6E3A1";
-          yellow = "#F9E2AF";
-          blue = "#89B4FA";
-          magenta = "#F5C2E7";
-          cyan = "#94E2D5";
-          white = "#BAC2DE";
-        };
-        bright = {
-          black = "#585B70";
-          red = "#F38BA8";
-          green = "#A6E3A1";
-          yellow = "#F9E2AF";
-          blue = "#89B4FA";
-          magenta = "#F5C2E7";
-          cyan = "#94E2D5";
-          white = "#A6ADC8";
-        };
-        dim = {
-          black = "#45475A";
-          red = "#F38BA8";
-          green = "#A6E3A1";
-          yellow = "#F9E2AF";
-          blue = "#89B4FA";
-          magenta = "#F5C2E7";
-          cyan = "#94E2D5";
-          white = "#BAC2DE";
-        };
-        selection = {
-          text = "#1E1E2E";
-          background = "#F5E0DC";
-        };
-        hints = {
-          start = {
-            foreground = "#1E1E2E";
-            background = "#F9E2AF";
-          };
-          end = {
-            foreground = "#1E1E2E";
-            background = "#A6ADC8";
-          };
         };
         cursor = {
           text = "#1E1E2E";
@@ -111,6 +67,43 @@ in
         footer_bar = {
           foreground = "#1E1E2E";
           background = "#A6ADC8";
+        };
+
+        hints = {
+          start = {
+            foreground = "#1E1E2E";
+            background = "#F9E2AF";
+          };
+          end = {
+            foreground = "#1E1E2E";
+            background = "#A6ADC8";
+          };
+        };
+
+        selection = {
+          text = "#1E1E2E";
+          background = "#F5E0DC";
+        };
+
+        normal = {
+          black = "#45475A";
+          red = "#F38BA8";
+          green = "#A6E3A1";
+          yellow = "#F9E2AF";
+          blue = "#89B4FA";
+          magenta = "#F5C2E7";
+          cyan = "#94E2D5";
+          white = "#BAC2DE";
+        };
+        bright = {
+          black = "#585B70";
+          red = "#F38BA8";
+          green = "#A6E3A1";
+          yellow = "#F9E2AF";
+          blue = "#89B4FA";
+          magenta = "#F5C2E7";
+          cyan = "#94E2D5";
+          white = "#A6ADC8";
         };
 
         indexed_colors = [
