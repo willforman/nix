8d2ab3e069405a957fa6db316ac69c1c31b3a180
willforman <wf8581@gmail.com>
2024-11-24 16:24:12 -0600

    nvim: Make trouble more lazy-loadable

--- home-manager/lib/nvim/config/lua/plugins/trouble.lua
+++ home-manager/lib/nvim/config/lua/plugins/trouble.lua
@@ -1,16 +1,15 @@
 local M = {
   'folke/trouble.nvim',
-  cmd = { 'Trouble' }
+  cmd = { 'Trouble' },
+  keys = {
+    { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Open Trouble - all buffers' },
+    { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Open Trouble - this buffer' },
+  },
 }
 
 function M.config()
   local trouble = require('trouble')
   trouble.setup({})
-end
-
-function M.init()
-  vim.keymap.set('n', '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', { desc = 'Open Trouble - all buffers' })
-  vim.keymap.set('n', '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', { desc = 'Open Trouble - this buffer' })
 end
 
 return M
