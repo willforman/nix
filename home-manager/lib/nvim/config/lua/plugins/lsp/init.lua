5a8c6be5a67e9b3672df5b6f8d7e0f9cfd4de159
willforman <wf8581@gmail.com>
2024-12-29 19:12:54 -0600

    Finish refactor of LSP code

--- home-manager/lib/nvim/config/lua/plugins/lsp/init.lua
+++ home-manager/lib/nvim/config/lua/plugins/lsp/init.lua
@@ -1,3 +1,54 @@
+--@type lspconfig.options
+local servers = {
+  lua_ls = {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim' },
+        }
+      }
+    }
+  },
+  ts_ls = {},
+  eslint = {},
+  pyright = {},
+  gopls = {},
+  rust_analyzer = {
+    settings = {
+      ['rust-analyzer'] = {
+        cargo = {
+          allFeatures = true,
+          allTargets = true,
+          loadOutDirsFromCheck = true,
+          runBuildScripts = true,
+        },
+        checkOnSave = {
+          allFeatures = true,
+          command = "clippy",
+        },
+        diagnostics = false,
+      }
+    }
+  },
+  svelte = {},
+  html = {},
+  cssls = {},
+  elixirls = {
+    cmd = { 'elixir-ls' },
+  },
+  nixd = {},
+  bashls = {},
+  ruff = {
+    init_options = {
+      settings = {
+        args = { '--config', 'CONFIG_HERE' },
+      }
+    },
+  },
+  fennel_ls = {},
+  roc_ls = {},
+}
+
 return {
   "neovim/nvim-lspconfig",
   event = "UIEnter",
@@ -11,18 +62,7 @@ return {
       diagnostics = {
         underline = true,
       },
-      --@type lspconfig.options
-      servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { 'vim' },
-              }
-            }
-          }
-        },
-      }
+      servers = servers,
     }
 
     return ret
@@ -30,39 +70,22 @@ return {
   --param ops PluginLspOpts
   config = function (_, opts)
     local lspconfig = require("lspconfig")
+
+    local function on_attach(client, bufnr)
+      require('plugins.lsp.keys').init(client, bufnr)
+    end
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+    local options = {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+
     for server, server_opts in pairs(opts.servers) do
+      server_opts = vim.tbl_deep_extend("force", {}, options, server_opts)
       lspconfig[server].setup(server_opts)
     end
   end
 }
-
--- local M = {
---   'neovim/nvim-lspconfig',
---   name = 'lsp',
---   event = 'UIEnter',
---   dependencies = {
---     'hrsh7th/cmp-nvim-lsp'
---   }
--- }
---
--- function M.config()
---   local function on_attach(client, bufnr)
---     require('plugins.lsp.keys').init(client, bufnr)
---   end
---
---   local capabilities = vim.lsp.protocol.make_client_capabilities()
---   capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
---
---   local options = {
---     on_attach = on_attach,
---     capabilities = capabilities,
---   }
---
---   local servers = require('plugins.lsp.servers').servers
---   for server, opts in pairs(servers) do
---     opts = vim.tbl_deep_extend("force", {}, options, opts)
---     require('lspconfig')[server].setup(opts)
---   end
--- end
---
--- return M
