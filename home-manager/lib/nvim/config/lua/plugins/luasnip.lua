f1d211eed43e6b35a0995157b83053fa8fea2cd4
willforman <wf8581@gmail.com>
2025-07-16 07:01:33 -0500

    Add friendly snippets

--- home-manager/lib/nvim/config/lua/plugins/luasnip.lua
+++ home-manager/lib/nvim/config/lua/plugins/luasnip.lua
@@ -2,7 +2,8 @@ return {
   'L3MON4D3/LuaSnip',
   version = "v2.*",
   event = 'InsertEnter',
+  dependencies = { "rafamadriz/friendly-snippets" },
   config = function ()
-    require('luasnip').setup({})
+    require("luasnip.loaders.from_vscode").lazy_load()
   end
 }
