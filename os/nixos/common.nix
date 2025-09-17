5c5d1941f0cdde07f0e838ed0ee9dd46bb32df61
willforman <wf8581@gmail.com>
2025-09-16 20:47:23 -0500

    Add new public ssh keys

--- os/nixos/common.nix
+++ os/nixos/common.nix
@@ -14,8 +14,8 @@
     will = {
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEd+kmG9ScsNZZtqNRnhsBIBBSM5sv/ma8cuHTjGU6UQ wf8581@gmail.com" # mbp
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA3E5qb0rP7crUGLtOoztcPvXZ42P8QQMNXZrNXDxWKi wf8581@gmail.com" # desktop
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINT6g7uwS7pKQ1XKmfGCuwb2RUgHOYKYWCV7fKrh34X6 wf@willforman.com
+" # mbp
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFSSsTq5FjE5T6k0XYjulF89EHiYl4XRt9sMuIE7ah0N wf8581@gmail.com" # dev
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH4rvLHPVeJWl8WHQimBAAIv8fPykLhUphwbwH+fk/oG wf@willforman.com" # mbp2
       ];
