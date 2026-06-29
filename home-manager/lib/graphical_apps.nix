799aa738f17d40b5352cc9d26ff63958b5a746bb
willforman <wf8581@gmail.com>
2026-06-29 11:13:36 -0500

    Only use ghostty-bin for darwin

--- home-manager/lib/graphical_apps.nix
+++ home-manager/lib/graphical_apps.nix
@@ -6,7 +6,7 @@ in
 {
   programs.ghostty = {
     enable = true;
-    package = pkgs.ghostty-bin;
+    package = lib.mkIf (pkgs.stdenv.hostPlatform.system == "aarch64-darwin") pkgs.ghostty-bin;
   };
 
   xdg.configFile."ghostty/config".source = mkOutOfStoreSymlink "${config.home.homeDirectory}/code/nix/home-manager/lib/ghostty-config";
